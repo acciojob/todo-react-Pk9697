b91d7c726271d4db1b1158438514d4356e44fc6e
Prashant Kumar <prashant.aman9@gmail.com>
2024-07-19 13:38:27 +0530

    Complete Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,31 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [todo,setTodo] = useState('');
+
+  const handleAddTodo = () => {
+    if (todo.trim()) {
+      setTodos((prev) => [...prev, { id: Date.now(), text: todo }]);
+      setTodo('');
+    }
+  }
+
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="main">
+      {/* Do not remove the main div */}
+      <h1>To-Do List</h1>
+      <input name="todo" value={todo} onChange={(e)=>setTodo(e.target.value)} type="text" placeholder="Enter Todo" />
+      <button onClick={handleAddTodo}>Add Todo</button>
+
+      {todos.map((todo) => (
+        <div key={todo.id} className="todo">
+          <p>{todo.text}</p>
+          <button onClick={() => setTodos(todos.filter((t) => t.id !== todo.id))}>Delete</button>
+        </div>
+      ))}
     </div>
   )
 }
